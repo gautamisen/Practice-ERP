eb36f7aa446ea83e2e8dd09414fdb231ead59e05
gautamisen <sengautami930@gmail.com>
2024-11-22 21:29:42 +0530

    init project

---
+++ src/main/java/com/prep/PracticeErpApplication.java
@@ -0,0 +1,13 @@
+package com.prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeErpApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/prep/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.prep;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PracticeErpApplication.class);
+	}
+
+}
